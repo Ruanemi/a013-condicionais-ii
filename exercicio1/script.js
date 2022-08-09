5fa2dc2c1fbe632d062bd29333dd3ec8629425e2
Ruan <ruanemidio1412@gmail.com>
2022-08-09 18:39:05 -0300

    Exercicio de condicional 2 da labenu

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,24 @@
-const num = //seu numero aqui
+const num = +prompt('Coloque o numero a baixo')
+
+// if(num %2 === 0){
+//     if(num %3 == 0){
+//     alert('esse número é divisivel por 2 e por 3')
+//     console.log('esse número é divisivel por 2 e por 3')
+// }else{ 
+//     alert('esse número NÃO É divisivel por 2 e por 3')
+//     console.log('esse número NÃO É divisivel por 2 e por 3')
+// }}
+
+if(num){
+    console.log(num%2 === 0? 'O número é divisivel por 2' : '')
+    if(num % 3 === 0) {
+      console.log('O número é divisivel por 3')
+    }
+  }
+
+// if(num % 2 === 0 || num % 3 === 0) {
+//     console.log(num%2===0? 'Esse número é divisível por 2.':'Esse número é divisível por 3.')
+//     console.log(num%3===0? 'Esse número é divisível por 3.':'')
+//   }
+    
 
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,13 +1,18 @@
 //código a ser reescrito
 
-if(nome === "José"){
-	console.log("Oi, Zé!");
-} else {
-	console.log("Olá, "+nome);
-}
+// if(nome === "José"){
+// 	console.log("Oi, Zé!");
+// } else {
+// 	console.log("Olá, "+nome);
+// }
+nome = 'ruan'
+nome === 'josé'? console.log('oi, zé') : 
+console.log('olá, ' +nome)
 
-if(idade >= 18){
-	console.log("pode tirar carteira de motorista!");
-} else {
-	console.log("Ainda não pode tirar carteira de motorista");
-}
+idade = 17
+idade >= 18? console.log('pode tirar carteira de motorista'): console.log('Ainda não pode tirar carteira de motorista')
+// idade === >= 18 
+// 	console.log("pode tirar carteira de motorista!");
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista");
+// }
